cd2b4a8472256e5ff018abe9bf60af147b168ffc
Felix <felix.ruedlin@gmx.de>
2025-09-23 14:19:12 +0200

    Changed qlever connector

--- src/qlever_connector.rs
+++ src/qlever_connector.rs
@@ -1,20 +1,201 @@
+use std::collections::HashMap;
 use reqwest::Client;
 use serde::Deserialize;
-use std::env;
+use std::{env, fs};
 use std::error::Error;
+use std::fs::File;
+use std::io::Write;
 use std::process::Command;
+use bollard::Docker;
+use glob::glob;
+use regex::Regex;
+use serde_json::Value;
 use tokio::runtime::Runtime;
 
 pub struct QLeverConnection {
-    host: String,
-    qlever_file: String,
+    docker_id: String,
+    qlever_file: QleverFile
 }
 
 impl QLeverConnection {
+    
+    pub fn new(dataset: &String) -> Result<QLeverConnection, Box<dyn Error>> {
+        let mut qlever_file = QLeverConnection::setup_config(dataset);
+        qlever_file.replace_internal_variables();
+        // Create directory
+        fs::create_dir(qlever_file.data.get("NAME").unwrap().as_str())?;
+        QLeverConnection::get_data(&qlever_file);
+        QLeverConnection::index(&qlever_file);
+        let conn = QLeverConnection::start(&qlever_file);
+        Ok(conn)
+    }
+    
+    pub fn setup_config(dataset: &String) -> QleverFile {
+
+        let target = match dataset.as_str() {
+            "dblp" => Some("https://raw.githubusercontent.com/ad-freiburg/qlever-control/refs/heads/main/src/qlever/Qleverfiles/Qleverfile.dblp"),
+            "osm-country" => Some("https://raw.githubusercontent.com/ad-freiburg/qlever-control/refs/heads/main/src/qlever/Qleverfiles/Qleverfile.osm-country"),
+            _ => None,
+        };
+        
+        if let Some(target) = target {
+            let response = reqwest::blocking::get(target).unwrap();
+            let content = response.text().unwrap();
+            let mut qlever = toml::from_str::<QleverFile>(&content).unwrap();
+            return qlever
+        }
+        panic!("Missing config")
+    }
+    
+    pub fn get_data(qlever_file: &QleverFile) {
+        command_assist("bash", &["-c", qlever_file.data.get("GET_DATA").unwrap().as_str()], qlever_file.data.get("NAME").unwrap().as_str()).unwrap()
+    }
+    
+    pub fn index(qlever_file: &QleverFile) {
+        // create settings json
+        let name = qlever_file.data.get("NAME").unwrap().as_str();
+        let path = format!("{name}/{name}.settings.json");
+        let mut file = File::create(path).unwrap();
+        file.write_all(qlever_file.index.get("SETTINGS_JSON").unwrap().as_str().as_bytes()).unwrap();
+        // Create Index
+        fs::create_dir(format!("{name}/index").as_str()).unwrap();
+        let mut command = format!{
+            "docker run --rm -u $(id -u):$(id -g) \
+            -v /etc/localtime:/etc/localtime:ro \
+            -v {name}/index:/index \
+            -w /index \
+            --name qlever.index.{name} \
+            --init \
+            --entrypoint bash \
+            docker.io/adfreiburg/qlever:latest \
+            -c '"
+        };
+        // Add files arguments
+        if qlever_file.index.contains_key("MULTI_INPUT_JSON") {
+            command += "IndexBuilderMain \
+            -i {name} \
+            -s {name}.settings.json \
+            --vocabulary-type on-disk-compressed";
+            let json: Value = serde_json::from_str(&qlever_file.data.get("MULTI_INPUT_JSON").unwrap()).unwrap();
+            for file in glob(json["for-each"].as_str().unwrap()).unwrap() {
+                let file_path = file.unwrap().as_path().to_str().unwrap().to_string();
+                let cmd = json["cmd"].as_str().unwrap();
+                command += format!(" -f <({cmd} {file_path}) -g - -F ttl -p false").as_str()
+            }
+        } else {
+            command += qlever_file.index.get("CAT_INPUT_FILES").unwrap().as_str();
+            let stxxl = qlever_file.index.get("STXXL_MEMORY").unwrap();
+            command += format!(
+                " IndexBuilderMain \
+                -i {name} \
+                -s {name}.settings.json \
+                --vocabulary-type on-disk-compressed -F ttl -f - \
+                --stxxl-memory {stxxl}\
+                "
+            ).as_str()
+        }
+
+        command += format!(" | tee {name}.index-log.txt'").as_str();
+
+        command_assist("bash", &["-c", command.as_str()], name).unwrap()
+    }
+    
+    pub fn start(qlever_file: &QleverFile) -> QLeverConnection {
+        // docker run -d --restart=unless-stopped 
+        // -u $(id -u):$(id -g) 
+        // -v /etc/localtime:/etc/localtime:ro 
+        // -v $(pwd):/index 
+        // -p 7015:7015 
+        // -w /index 
+        // --name qlever.server.dblp 
+        // --init 
+        // --entrypoint bash 
+        // docker.io/adfreiburg/qlever:latest -c 
+        // 'ServerMain -i dblp -j 8 -p 7015 -m 10G -c 5G -e 1G -k 200 -s 300s -a dblp_yGJxTdx6CXRb > dblp.server-log.txt 2>&1'
+        let name = qlever_file.data.get("NAME").unwrap().as_str();
+        let uid = String::from_utf8(
+            Command::new("id")
+                .arg("-u")
+                .output()
+                .unwrap()
+                .stdout
+        ).unwrap();
+        let gid = String::from_utf8(
+            Command::new("id")
+                .arg("-g")
+                .output()
+                .unwrap()
+                .stdout
+        ).unwrap();
+        let wd = format!("{name}/index");
+        let port = qlever_file.server.get("PORT").unwrap().as_str();
+        
+        let mut command = format!(
+            "docker run -d --restart=unless-stopped \
+            -u {uid}:{gid} \
+            -v /etc/localtime:/etc/localtime:ro \
+            -v {wd}:/index \
+            -p {port}:{port} \
+            -w /index \
+            --name qlever.server.{name} \
+            --init \
+            --entrypoint bash \
+            docker.io/adfreiburg/qlever:latest -c 'ServerMain -i {name} -j 8 -p {port} "
+        );
+        if let Some(q_mem) = qlever_file.server.get("MEMORY_FOR_QUERIES") {
+            command += format!("-m {q_mem}").as_str();
+        } else {
+            command += "-m 5G";
+        }
+        if let Some(cache_max) = qlever_file.server.get("CACHE_MAX_SIZE") {
+            command += format!("-c {cache_max}").as_str();
+        } else {
+            command += "-c 5G";
+        }
+        if let Some(entry_max) = qlever_file.server.get("CACHE_MAX_SIZE_SINGLE_ENTRY") {
+            command += format!("-e {entry_max}").as_str();
+        } else {
+            command += "-e 1G";
+        }
+        if let Some(entry_count) = qlever_file.server.get("CACHE_MAX_NUM_ENTRIES") {
+            command += format!("-k {entry_count}").as_str();
+        } else {
+            command += "-k 200";
+        }
+        if let Some(timeout) = qlever_file.server.get("TIMEOUT") {
+            command += format!("-s {timeout}").as_str();
+        }
+        command += format!("> {name}.server-log.txt 2>&1'").as_str();
+        command_assist("bash", &["-c", command.as_str()], name).unwrap();
+        QLeverConnection {
+            qlever_file: qlever_file.clone(),
+            docker_id: format!("qlever.server.{name}")
+        }
+    }
+    
+    pub fn stop(&self) -> Result<(), Box<dyn Error>> {
+        //Stop docker container
+        let rt = Runtime::new().unwrap();
+        let handle = rt.handle();
+
+        handle.block_on(async {
+            let docker = Docker::connect_with_defaults().unwrap();
+
+            docker.stop_container(
+                self.docker_id.as_str(),
+                None::<bollard::query_parameters::StopContainerOptions>
+            ).await.unwrap();
+
+            docker.remove_container(self.docker_id.as_str(), None::<bollard::query_parameters::RemoveContainerOptions>).await.unwrap();
+        });
+
+        Ok(())
+    }
+
     pub fn run_test_query(&mut self, query: &str, rows: usize, columns: usize) -> Result<u128, Box<dyn Error>> {
         let rt = Runtime::new()?;
         let handle = rt.handle();
-        
+
         let result: (u128, usize, usize) = handle.block_on(self.do_query_request(query)).expect("query failed");
         if result.1 != rows || result.2 != columns {
             return Err(format!(
@@ -28,34 +209,9 @@ impl QLeverConnection {
         Ok(result.0)
     }
     
-    pub fn new(dataset: &String) -> Result<QLeverConnection, Box<dyn Error>> {
-        let qlever_file = match dataset.as_str() {
-            "dblp" => "dblp".to_string(),
-            _ => return Err("Invalid dataset for qlever".into()),
-        };
-        let mut conn = QLeverConnection { host: "http://localhost:7027".into() , qlever_file };
-        conn.startup()?;
-        Ok(conn)
-    }
-    
-    fn startup(&mut self) -> Result<(), Box<dyn Error>> {
-        command_assist("mkdir", &[format!("./src/data/{}", &self.qlever_file).as_str()], "/usr/src/bachelor_thesis")?;
-        command_assist_qlever(&["setup-config", format!("{}", &self.qlever_file).as_str()], &self.qlever_file)?;
-        command_assist_qlever(&["get-data"], &self.qlever_file)?;
-        command_assist_qlever(&["index"], &self.qlever_file)?;
-        command_assist_qlever(&["start"], &self.qlever_file)?;
-        Ok(())
-    }
-     
-    pub fn stop(self) -> Result<(), Box<dyn Error>> {
-        command_assist_qlever(&["stop"], &self.qlever_file)?;
-        command_assist("rm", &["-r", format!("./src/data/{}", &self.qlever_file).as_str()], "/usr/src/bachelor_thesis")?;
-        Ok(())
-    }
-    
     async fn do_query_request(&mut self, query: &str) -> Result<(u128, usize, usize), Box<dyn Error>> {
         let client = Client::new();
-        let response = client.post(self.host.clone())
+        let response = client.post("http://localhost:".to_string() + self.qlever_file.server.get("PORT").unwrap().as_str())
             .header("Accept", "application/qlever_results+json")
             .header("Content-Type", "application/sparql-query")
             .body(query.to_string())
@@ -74,6 +230,86 @@ impl QLeverConnection {
         Ok((time, result.runtime.query_execution_tree.result_rows, result.runtime.query_execution_tree.result_cols))
     }
 }
+
+#[derive(Deserialize, Clone)]
+struct QleverFile {
+    pub data: HashMap<String, String>,
+    pub index: HashMap<String, String>,
+    pub server: HashMap<String, String>,
+}
+
+impl QleverFile {
+    pub fn replace_internal_variables(&mut self) {
+        let regex = Regex::new(r"\$\{(?<prefix>\w+:)?(?<key>\w+)}").unwrap();
+        // Iterate over Data
+        self.data = self.data.iter().map(|(orig_key, value)| {
+            let mut changed = value.clone();
+            while regex.is_match(changed.as_str()) {
+                for cap in regex.captures_iter(value) {
+                    let matched = cap.get(0).unwrap().as_str();
+                    let prefix = cap.name("prefix");
+                    let key = cap.name("key").unwrap().as_str();
+                    let replacer = match prefix {
+                        Some(prefix) if prefix.as_str() == "data" => self.data.get(key),
+                        Some(prefix) if prefix.as_str() == "index" => self.index.get(key),
+                        Some(prefix) if prefix.as_str() == "server" => self.server.get(key),
+                        None | Some(_) => self.data.get(key)
+                    };
+                    
+                    if let Some(replacement) = replacer {
+                        changed = changed.replace(matched, replacement.as_str());
+                    }
+                }
+            }
+            (orig_key.to_string(), changed)
+        }).collect();
+        // Iterate over Index
+        self.index = self.index.iter().map(|(orig_key, value)| {
+            let mut changed = value.clone();
+            while regex.is_match(changed.as_str()) {
+                for cap in regex.captures_iter(value) {
+                    let matched = cap.get(0).unwrap().as_str();
+                    let prefix = cap.name("prefix");
+                    let key = cap.name("key").unwrap().as_str();
+                    let replacer = match prefix {
+                        Some(prefix) if prefix.as_str() == "data" => self.data.get(key),
+                        Some(prefix) if prefix.as_str() == "index" => self.index.get(key),
+                        Some(prefix) if prefix.as_str() == "server" => self.server.get(key),
+                        None | Some(_) => self.index.get(key)
+                    };
+
+                    if let Some(replacement) = replacer {
+                        changed = changed.replace(matched, replacement.as_str());
+                    }
+                }
+            }
+            (orig_key.to_string(), changed)
+        }).collect();
+        // Iterate over Server
+        self.server = self.server.iter().map(|(orig_key, value)| {
+            let mut changed = value.clone();
+            while regex.is_match(changed.as_str()) {
+                for cap in regex.captures_iter(value) {
+                    let matched = cap.get(0).unwrap().as_str();
+                    let prefix = cap.name("prefix");
+                    let key = cap.name("key").unwrap().as_str();
+                    let replacer = match prefix {
+                        Some(prefix) if prefix.as_str() == "data" => self.data.get(key),
+                        Some(prefix) if prefix.as_str() == "index" => self.index.get(key),
+                        Some(prefix) if prefix.as_str() == "server" => self.server.get(key),
+                        None | Some(_) => self.server.get(key)
+                    };
+
+                    if let Some(replacement) = replacer {
+                        changed = changed.replace(matched, replacement.as_str());
+                    }
+                }
+            }
+            (orig_key.to_string(), changed)
+        }).collect();
+    }
+}
+
 
 // Response deserialization struct
 #[derive(Deserialize)]
