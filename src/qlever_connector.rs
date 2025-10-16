398369864c23d676c3b830646b7f751c60dc9440
Felix <felix.ruedlin@gmx.de>
2025-10-16 15:39:14 +0200

    Wait for server to start

--- src/qlever_connector.rs
+++ src/qlever_connector.rs
@@ -5,6 +5,7 @@ use std::fs;
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
+use std::net::IpAddr;
 use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
@@ -239,7 +240,7 @@ impl QLeverConnection {
         let client = reqwest::blocking::Client::builder()
             .redirect(reqwest::redirect::Policy::none())
             .no_proxy()
-            .local_address("127.0.0.1")
+            .local_address(IpAddr::from([127, 0, 0, 1]))
             .build()
             .unwrap();
         let query = query.to_string();
